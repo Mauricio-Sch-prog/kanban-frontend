a87a7eabd40bb3bb9711c9c662e2d1fb7bdede0b
Programador_Gaúcho <mauricio.ass.2016@gmail.com>
2026-08-05 21:31:52 -0300

    connect to api

---
+++ src/services/api.ts
@@ -0,0 +1,25 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export async function apiFetch(
+  path: string,
+  options: RequestInit = {}
+) {
+
+  console.log(`${API_URL}${path}`);
+  
+
+  const res = await fetch(`${API_URL}${path}`, {
+    ...options,
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    },
+  });
+
+  if (!res.ok) {
+    throw await res.json();
+  }
+
+  return res.json();
+}
