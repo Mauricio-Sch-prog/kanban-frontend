a3904cd33e362f27d037fd08c476a3b726a1d048
Programador_Gaúcho <mauricio.ass.2016@gmail.com>
2026-08-14 10:09:23 -0300

    add types

---
+++ src/types/board.ts
@@ -0,0 +1,8 @@
+export type Board = {
+  id: string;
+  name: string;
+  positionX: number;
+  positionY: number;
+  height: number;
+  width: number;
+};
---
+++ src/types/lane.ts
@@ -0,0 +1,5 @@
+export type Lane = {
+  id: string;
+  name: string;
+  index: number;
+};
