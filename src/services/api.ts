d5641f9da286c77a417611a688ebdd4dbb6d55dd
Programador_Gaúcho <mauricio.ass.2016@gmail.com>
2026-08-17 15:28:55 -0300

    better api call error handling

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,20 +1,34 @@
+import { ErrorResponse } from '@/types/error.response';
+
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function apiFetch(path: string, options: RequestInit = {}) {
   console.log(`${API_URL}${path}`);
 
-  const res = await fetch(`${API_URL}${path}`, {
-    ...options,
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-      ...(options.headers || {}),
-    },
-  });
+  try {
+    const res = await fetch(`${API_URL}${path}`, {
+      ...options,
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        ...(options.headers || {}),
+      },
+    });
 
-  if (!res.ok) {
-    throw await res.json();
+    return res.json();
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw error;
+    }
+    if (typeof error === 'object' && error !== null && 'message' in error) {
+      const apiErr = error as ErrorResponse;
+      const message = Array.isArray(apiErr.message)
+        ? apiErr.message.join(', ')
+        : apiErr.error || 'An unexpected error occurred';
+
+      throw new Error(message);
+    }
+
+    throw new Error('An unexpected error occurred');
   }
-
-  return res.json();
 }
